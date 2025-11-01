05c42b827758698141eba15dd8386f830517d668
KK <khaing@anbgroup.com>
2025-11-01 10:54:51 +0630

    change global

--- src/Global.js
+++ src/Global.js
@@ -1,4 +1,4 @@
 // Use an environment variable for the API base URL so it can differ between
 // development (local json-server) and production (real API). If REACT_APP_API_BASE_URL
 // is not set the code will default to the local json-server used during development.
-export const appBaseUrl = '/cyber_ninja_community_01' || 'http://localhost:3005/';
+export const appBaseUrl = 'https://whitebunny94.github.io/cyber_ninja_community_01/' || 'http://localhost:3005/';
